a0ccfadb4b33c13a0aa92315767ccc4254cbcd6e
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-09-14 20:45:50 -0300

    'att5'

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const consign = require("consign")
 const app = express()
 const path = require("path")
 const bodyparser = require("body-parser")
-const { cachedDataVersionTag } = require("v8")
 const dir = __dirname
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -22,7 +21,42 @@ var io = require('socket.io')(http);
 var Users = []
 
 //  Rotas
-consign().include("Rotas").into(app, dir, io, Users)
+//consign().include("Rotas").into(app, dir, io, Users)
+
+// Rotas
+app.get("/", function(req, res){
+    res.sendFile("./paginas/views/index.html")
+})
+app.get("/controle", function(req, res){
+    res.sendFile("./paginas/views/controle.html")
+})
+
+io.on('connection', function(socket){
+
+    // Links
+    socket.on('newUser', function(session){
+        Users.push({id:socket.id, session:session})
+    });
+    socket.on('desenhar', function(session, pincel){
+        io.to(idIo(session)).emit('desenhar', pincel);
+    });
+    socket.on('QrCode', function(session){
+        io.to(idIo(session)).emit('QrCode');
+    });
+    socket.on('screen', function(session, width, height){
+        io.to(idIo(session)).emit('screen', width, height);
+    });
+    
+    // Functions
+    function idIo(session){
+        for(var i=0; i<Users.length; i++) {
+            if(Users[i].session == session) {
+                return Users[i].id;
+            }
+        }
+    }
+
+});
 
 // Porta
 http.listen(443, "localhost", function(){
